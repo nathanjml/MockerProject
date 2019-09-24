e761b277ff20cb082ad51ea6c227aacd75e8e5bd
Nathan J Murphy <nmurphy@turner-industries.com>
2019-09-24 13:40:22 -0500

    feat(initial)

---
+++ Mocker.Core/Mocker.cs
@@ -0,0 +1,11 @@
+﻿namespace Mocker.Core
+{
+    public static class Mocker
+    {
+        public static MockerInstance<T> ForEntity<T>()
+            where T : class, new()
+        {
+            return new MockerInstance<T>();
+        }
+    }
+}
---
+++ Mocker.Core/MockerInstance.cs
@@ -0,0 +1,50 @@
+﻿using Mocker.Core.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Mocker.Core
+{
+    public class MockerInstance<T>
+        where T : class, new()
+    {
+        internal MockerInstance() { }
+
+        public List<Expression<Action<T>>> expressionObjIntializers = new List<Expression<Action<T>>>();
+
+        public ExpressionEvaluator<T, Z> Prop<Z>(Expression<Func<T, Z>> entityExpression)
+        {
+            var expEvaluator = new ExpressionEvaluator<T, Z>(this,  entityExpression);
+            return expEvaluator;
+        }
+
+        public T Make()
+        {
+            var instance = new T();
+
+            foreach(var e in expressionObjIntializers)
+            {
+                e.Compile().Invoke(instance);
+            }
+
+            return instance;
+        }
+
+        public IList<T> Make(int numberToMake)
+        {
+            var tList = new List<T>(numberToMake);
+
+            for(var i = 0; i<numberToMake; i++)
+            {
+                var instance = new T();
+                foreach(var e in expressionObjIntializers)
+                {
+                    e.Compile().Invoke(instance);
+                }
+                tList.Add(instance);
+            }
+
+            return tList;
+        }
+    }
+}
